4fbccbb482e7c89f94d9919776e374a9b6379328
venumadhav17 <kalyanamvenumadhav@gmail.com>
2024-06-13 19:14:23 +0530

    Laying the foundation

--- App.js
+++ App.js
@@ -1,76 +1,67 @@
-// React-18 stuff
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-/*
-<div id="parent">
-       <div id="child">
-             <h1>Hello</h1>
-        </div>
-</div>
-*/
+// React.createElement -> React Element -> Object -> HTML Element(Renders into DOM)
+// core element in React
+const heading = React.createElement('h1', { id: 'heading' }, 'Namaste React');
+// use fragments or array for multiple lines in JSX
+const jsx = [<h1>Class</h1>, <h2>Hello</h2>];
 
-// ReactElement(Object) => HTML(Browser Understands)
+// JSX is not HTML in JS -> It's HTML(XML) like Syntax
+// JSX(transpiled) before it return the JS Code -> Parcel -> Babel(package)
+// JSX => Babels transpiles -> React.createElement => React Element => Object => HTML Element
+const jsxHeading = <h1 id="heading">Namaste React using JSX</h1>;
 
-/*
-How to create Sibblings
-------------------------
+// React Component
+// Class Based Components -> Older Way - Uses Javascript Classes
+// functional Component -> Newer Way - Uses Javascript Functions
 
-<div id="parent">
-       <div id="child">
-             <h1>Hello</h1>
-             <h2>Hello1</h2>
-        </div>
-</div>
-*/
+// React Functional Component (React Javascript function)
 
-/*
-<div id="parent">
-       <div id="child">
-             <h1>Hello</h1>
-             <h2>Hello1</h2>
-        </div>
-        <div id="child">
-             <h1>Hello</h1>
-             <h2>Hello1</h2>
-        </div>
-</div
+// const fn = () => true;
 
-*/
+/*const HeadingComponent = () => {
+  return <h1 className="heading">Namaste React Functional Component</h1>;
+}; */
 
-const parent = React.createElement(
-  'div',
-  { id: 'parent' },
-  React.createElement('div', { id: 'child' }, [
-    React.createElement('h1', {}, 'Im an h1 tag'),
-    React.createElement('h2', {}, 'Im an h2 tag'),
-    React.createElement('h1', {}, 'Im an h1 tag'),
-    React.createElement('h2', {}, 'Im an h2 tag'),
-  ])
+// Component Composition - composing 2 components into one another
+/*const Title = () => (
+  <h1 className="head" tabIndex="5">
+    Namaste React
+  </h1>
+); */
+
+const title = <h1>Hello</h1>;
+
+const data = getData();
+// Cross Side Scripting Attack
+// JSX can take of this attacks
+
+//const title = 10000;
+const HeadingComponent = () => (
+  <div id="container">
+    {12345 + 678910}
+    <Title />
+    <Title></Title>
+    {Title()}
+    {title}
+    {data}
+    {console.log('Hello')}
+    <h1 className="head" tabIndex="5">
+      Namaste React Using JSX
+    </h1>
+  </div>
 );
 
-/*const parent = React.createElement(
-  'div',
-  { id: 'parent' },
-  React.createElement('div', { id: 'child' }, [
-    React.createElement('h1', {}, 'Im an h1 tag'),
-    React.createElement('h2', {}, 'Im an h2 tag'),
-  ])
-); 
-console.log(parent);
-const parent = React.createElement(
-  'div',
-  { id: 'parent' },
-  React.createElement(
-    'div',
-    { id: 'child' },
-    React.createElement('h1', {}, 'Im an h1 tag')
-  )
-); 
+// we can use functional component inside element
+// Don't put div(Component) inside h1
+/*const HeadingComponent = () => (
+  <h1 className="heading">Namaste React Functional Component</h1>
+);*/
 
-// 1st line comes form 1st cdn link
-const heading = React.createElement('h1', { id: 'heading' }, 'Hello React');
-// If we need to put heading into DOM / into browser we use ReactDom.createRoot
-// root is the place where react code is render
-console.log(heading); // Object */
+/* const HeadingComponent = () => (
+  <div>
+  Hello</div>) */
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent);
+root.render(<HeadingComponent />);
